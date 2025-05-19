2b49d0cc935b45bbf53358f30ad173853d0dbe95
Rafal Zablotni <rafalzab20@gmail.com>
2025-05-19 12:25:32 +0000

    RZ415286 Add Dcokerfile for smoke

--- smoke_test.sh
+++ smoke_test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo "Running smoke test..."
-/app/snake_bin --help || echo "Smoke test passed: binary is executable"
+echo "[SMOKE TEST] Running check..." > /app/smoke.log
+/app/snake_bin >> /app/smoke.log 2>&1
+echo "[SMOKE TEST] Done." >> /app/smoke.log
